055f824332e60091e260352ed5d04ccbdc7273a3
c t <20270456+chhayatuok@users.noreply.github.com>
2020-08-09 18:42:26 -0400

    Working. Need to start children containers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import MicroFrontend from './MicroFrontEnd'
 import './App.css';
+
+const defaultHistory = createBrowserHistory();
+
+const {
+  REACT_APP_DOGS_HOST: dogshost,
+  REACT_APP_CATS_HOST: catshost,
+} = process.env;
+
+function Header() {
+  return (
+    <div className="banner">
+      <h1 className="banner-title">Cats and Dogs</h1>
+      <h4>Random pics of cats and dogs</h4>
+    </div>
+  );
+}
+
+// ***START***
+//Creating MicfroFrontends
+// ***START***
+function Dogs({history}) {
+  return <MicroFrontend history={history} host={dogshost} name="Dogs" />;
+}
+
+function Cats({history}) {
+  return <MicroFrontend history={history} host={catshost} name="Cats" />;
+}
+
+function GreetingCat({history}) {
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <MicroFrontend history={history} host={catshost} name="Cats" />
+      </div>
+    </div>
+  )
+}
+// *** END ***
+//Creating MicfroFrontends
+// *** END ***
+
+function Home({history = defaultHistory}) {
+  const [input, setInput] = useState("");
+
+  const handleOnClick = () => {
+    history.push(`/cat/${input}`);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <input
+          placeholder="Insert a greeting"
+          defaultValue={input}
+          onBlur={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleOnClick}>Greet Me</button>
+      </div>
+
+      <div className="home">
+        <div className="content">
+          <div className="cat">
+            <Cats />
+          </div>
+          <div className="dog">
+            <Dogs />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cat/:greeting" component={GreetingCat} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
